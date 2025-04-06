fe17f5a1ceb0e8160b03c54d1f90cab5ef3da948
Karthiktm28 <karthiktm2804@gmail.com>
2025-04-06 01:50:44 +0100

    trigger webhook test

---
+++ src/test/java/com/bookstore/order_service/controller/OrderControllerTest.java
@@ -0,0 +1,26 @@
+package com.bookstore.order_service.controller;
+
+import com.bookstore.order_service.OrderServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetAllOrdersEndpoint() throws Exception {
+        mockMvc.perform(get("/orders"))
+               .andExpect(status().isOk());
+    }
+}
