87371691901a3cc939f5189cf91294fb4705bde8
Karthiktm28 <karthiktm2804@gmail.com>
2025-04-07 22:22:10 +0100

    chore: trigger Jenkins build

--- src/main/java/com/bookstore/order_service/OrderServiceApplication.java
+++ src/main/java/com/bookstore/order_service/OrderServiceApplication.java
@@ -12,6 +12,7 @@ public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
+// Trigger CI/CD pipeline
 
     @Bean
     @LoadBalanced
